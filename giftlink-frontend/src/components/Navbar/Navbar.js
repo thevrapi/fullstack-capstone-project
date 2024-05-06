0f169f53e1fb1da682adce7c572c5f077301f17f
The Vrapi <101073476+thevrapi@users.noreply.github.com>
2024-05-07 01:08:57 +0200

    implemented register code

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -1,7 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom'
+import {urlConfig} from '../../config';
+import { useAppContext } from '../../context/AuthContext';
 
 export default function Navbar() {
+    const { isLoggedIn, setIsLoggedIn, userName, setUserName } = useAppContext();
+    const navigate=useNavigate();
+      useEffect(() => {
+          const authTokenFromSession = sessionStorage.getItem('auth-token');
+          const nameFromSession = sessionStorage.getItem('name');
+          if (authTokenFromSession) {
+              if(isLoggedIn && nameFromSession) {
+                setUserName(nameFromSession);
+              } else {
+                sessionStorage.removeItem('auth-token');
+                sessionStorage.removeItem('name');
+                sessionStorage.removeItem('email');
+                setIsLoggedIn(false);
+              }
+          }
+      },[isLoggedIn, setIsLoggedIn, setUserName])
+      const handleLogout=()=>{
+          sessionStorage.removeItem('auth-token');
+          sessionStorage.removeItem('name');
+          sessionStorage.removeItem('email');
+          setIsLoggedIn(false);
+          navigate(`/app`);
+      }
+      const profileSecton=()=>{
+        navigate(`/app/profile`);
+      }
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <Link className="navbar-brand" to="/">GiftLink</Link>
@@ -11,6 +39,29 @@ export default function Navbar() {
                     <li className="nav-item"><Link className="nav-link" to="/app">Gifts</Link></li>
                     <li className="nav-item"><Link className="nav-link" to="/app/search">Search</Link></li>
                 </ul>
+                <ul className="navbar-nav ml-auto">
+                        {isLoggedIn ? (
+                            <>
+                                <li className="nav-item"> 
+                                    <span className="nav-link" style={{color: "black", cursor:"pointer"}} onClick={profileSecton}>
+                                        Welcome, {userName}
+                                    </span> 
+                                </li>
+                                <li className="nav-item">
+                                    <button className="nav-link login-btn" onClick={handleLogout}>Logout</button>
+                                </li>
+                            </>
+                        ) : (
+                            <>
+                                <li className="nav-item">
+                                    <Link className="nav-link login-btn" to="/app/login">Login</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link register-btn" to="/app/register">Register</Link>
+                                </li>
+                            </>
+                        )}
+                    </ul>
             </div>
         </nav>
     );
